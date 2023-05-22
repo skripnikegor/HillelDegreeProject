55220a1c2af9e24dfdef0aa6fe05da6177d4d444
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-05-22 21:01:08 +0300

    Added IRepository interface

---
+++ DegreeProject/DegreeProject.DB/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeProject.DB.Interfaces
+{
+    public interface IRepository <T>
+    {
+        T GetItemById(int Id);
+        void AddItem(T item);
+        void UpdateItem(T item);
+        void DeleteItem(int Id);
+    }
+}
