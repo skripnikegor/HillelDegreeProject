e4f346e249c2dc55fa7d565e3b3c8639b9724a5e
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-05-22 21:09:09 +0300

    Added DataContext

---
+++ DegreeProject/DegreeProject.DB/DataContexts/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DegreeProject.DB.DataContexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=UACHA1NB258\\SQLEXPRESS;Initial Catalog=DegreeProject;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
