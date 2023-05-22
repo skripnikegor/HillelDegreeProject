8cb6a0ef429b23cebf56488e1e87c327ffbc73a4
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-05-22 19:33:26 +0300

    Added UserBase class

---
+++ DegreeProject/DegreeProject.DB/Models/UserBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeProject.DB.Models
+{
+    public class UserBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
